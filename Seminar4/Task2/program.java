28f3c8523cf07f039d85013a6b6460d81aaa8e35
Aleksei <ais_80@inbox.ru>
2023-03-21 20:17:12 +0300

    Task2 done

--- Seminar4/Task2/program.java
+++ Seminar4/Task2/program.java
@@ -1,4 +1,7 @@
 package Seminar4.Task2;
+
+import java.util.LinkedList;
+
 /*
  * :
  * enqueue() — помещает элемент в конец очереди,
@@ -6,7 +9,23 @@ package Seminar4.Task2;
  * first() — возвращает первый элемент из очереди, не удаляя.
  */
 public class program {
+    public static void enqueue(LinkedList<Integer> list, int elem) {
+        list.add(elem);
+    }
+    public static int dequeue(LinkedList<Integer> list) {
+        return list.removeFirst();
+    }
+    public static int first(LinkedList<Integer> list) {
+        return list.getFirst();
+    }
     public static void main(String[] args) {
-        
+        LinkedList<Integer> myList= new LinkedList<Integer> ();
+        enqueue(myList, 5);
+        enqueue(myList, 6);
+        enqueue(myList, 7);
+        enqueue(myList, 8);
+        System.out.println(myList);
+        System.out.println(dequeue(myList));
+        System.out.println(first(myList));
     }
 }
